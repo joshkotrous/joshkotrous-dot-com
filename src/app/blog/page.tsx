5b6e80c23be125efd3bf3d5bd39d86fbde57dba9
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-10-05 00:18:53 -0500

    Fixed blog page z index

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,7 +8,7 @@ const Blog = async () => {
 
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen overflow-hidden px-4 pt-24 md:pt-20">
-      <main className="flex flex-col w-full  max-w-5xl gap-4">
+      <main className="flex flex-col w-full  max-w-5xl gap-4 pb-20">
         <BlogClient posts={posts} />
       </main>
     </div>
