6e0488feb5f67e94ee236691efce3fe02ec15543
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-10-07 23:59:27 -0500

    Added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://joshkotrous.com', // Replace with your site's URL
-    generateRobotsTxt: true,  // (optional) Generate a robots.txt file
-    exclude: ['/admin/*'], // (optional) Exclude certain pages from the sitemap
+    siteUrl: 'https://joshkotrous.com',
+    generateRobotsTxt: true, // Generate a robots.txt file
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    
+    // Exclude paths that end with .png, .jpg, .jpeg, etc.
+    exclude: [
+      '*.png', // Exclude all .png files
+      '*.jpg', // Exclude all .jpg files
+      '*.jpeg', // Exclude all .jpeg files
+      '*.svg', // Exclude all .svg files
+      '*.gif', // Exclude all .gif files
+    ],
   };
