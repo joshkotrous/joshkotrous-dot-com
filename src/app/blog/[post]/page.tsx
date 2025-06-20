03ddf7d96e7271fa146a7892432e08252d8e51af
joshkotrous <joshkotrous@gmail.com>
2025-06-20 14:37:18 -0400

    Testing og

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -51,7 +51,7 @@ export async function generateMetadata({
       type: "article",
       images: post.image
         ? [{ url: post.image, width: 1200, height: 630, alt: post.title }]
-        : "./opengraph-image.png",
+        : "./blog/opengraph-image.png",
     },
     twitter: {
       card: "summary_large_image",
@@ -59,7 +59,7 @@ export async function generateMetadata({
       description: post.description,
       images: post.image
         ? [{ url: post.image, width: 1200, height: 630, alt: post.title }]
-        : "./twitter-image.png",
+        : "./blog/twitter-image.png",
     },
   };
 }
